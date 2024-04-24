6f1ccaaafae54e2826282e9858f1316c66a63fd3
Ricardo Zuniga <144750809+imaginuz@users.noreply.github.com>
2024-04-24 11:03:17 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import handlebars from 'vite-plugin-handlebars';
 import hbsContext  from './context/context';
 export default defineConfig(
     {
-        base: '/Vite-Portfolio/',
+        base: '/Vite-Less-Portfolio/',
         appType: 'mpa',
         build: {
             rollupOptions: {
